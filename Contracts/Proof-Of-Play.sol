5a291b8b7badf9a6425a4abb8bdcfc2811c099cb
0xSorcerers <121747420+0xsorcerers@users.noreply.github.com>
2024-01-26 10:18:27 +0100

    Update Proof-Of-Play.sol

--- Contracts/Proof-Of-Play.sol
+++ Contracts/Proof-Of-Play.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "abdk-libraries-solidity/ABDKMath64x64.sol";
 
 interface IBattledog {
     struct Player {
